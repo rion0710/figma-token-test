3f173482145f2de0de8df4839f95442aee611e31
rion0710 <ohjunsang@inflearn.com>
2022-11-06 16:34:46 +0900

    INIT

---
+++ build/ios/StyleDictionarySize.h
@@ -0,0 +1,15 @@
+
+// StyleDictionarySize.h
+//
+
+// Do not edit directly
+// Generated on Sun, 06 Nov 2022 07:31:36 GMT
+
+
+#import <Foundation/Foundation.h>
+
+
+extern float const SizeFontSmall;
+extern float const SizeFontMedium;
+extern float const SizeFontLarge;
+extern float const SizeFontBase;
